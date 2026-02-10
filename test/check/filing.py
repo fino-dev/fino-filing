db3b9fb486a883e4a414d0216a346aeb40db5892
kudoakiya <kudoakiya@team-lab.com>
2026-02-10 21:42:04 +0900

    add sample doc

--- test/check/filing.py
+++ test/check/filing.py
@@ -1,5 +1,18 @@
-from fino_filing.filing.filing import EDINETFiling
+import datetime
 
-edf = EDINETFiling()
+from fino_filing import Filing
 
-print(edf)
+
+filing = Filing(
+    id="hoge_id",
+    source="test",
+    checksum="123",
+    name="test.zip",
+    is_zip=True,
+    created_at=datetime.time(),
+)
+
+print("class")
+print(Filing.id)
+print("instance")
+print(filing.id)
