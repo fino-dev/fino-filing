32157c66c4b88a5c22822e399404634a1269dfcc
Akiya-Kudo <k.akiya07@gmail.com>
2026-03-16 04:35:31 +0900

    add todo

--- test/scenario/collect_edger_facts/test_1.py
+++ test/scenario/collect_edger_facts/test_1.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import pytest
 
-from fino_filing import Collection, EdgerConfig, EdgerFactsCollector
+from fino_filing import Collection, EDGARFiling, EdgerConfig, EdgerFactsCollector, Field
 
 
 @pytest.mark.scenario
@@ -44,12 +44,42 @@ class TestScenario_CollectEdgerFacts:
         assert path is not None
         assert path.exists()
 
-        # Collectionから取得できる
+        # Collectionからgetできる
         filing, content, path = temp_collection.get(collected[0][0].id)
         assert filing is not None
         assert filing.source == "EDGAR"
         assert content is not None
 
         assert path is not None
-        # get() が返す path は絶対パス。add の戻り値も絶対パスのため一致する。
         assert Path(path).resolve() == Path(collected[0][1]).resolve()
+
+        # Collectionからsearchできる
+        filings = temp_collection.search(expr=(Field("source") == EDGARFiling.source))
+        filing = filings[0]
+        assert filing is not None
+        assert isinstance(filing, EDGARFiling)
+        assert filing.source == "EDGAR"
+        assert filing.id == collected[0][0].id
+        assert filing.name == collected[0][0].name
+        assert filing.created_at == collected[0][0].created_at
+        assert filing.format == "json"
+        assert filing.is_zip is False
+
+        assert filing.cik == "0001652044"
+        assert filing.accession_number == "facts-0001652044"
+        assert filing.form_type == "companyfacts"
+        assert filing.company_name == "Alphabet Inc."
+
+        filings = temp_collection.search(expr=(EDGARFiling.cik == "0001652044"))
+        filing = filings[0]
+        assert filing is not None
+        assert isinstance(filing, EDGARFiling)
+        assert filing.source == "EDGAR"
+        assert filing.id == collected[0][0].id
+        assert filing.name == collected[0][0].name
+        assert filing.created_at == collected[0][0].created_at
+        assert filing.created_at.tzinfo is None
+        assert filing.created_at.tzinfo is None
+
+TODO: expreがどのようなケースに対応するか整理する必要あり
+TODO: ID genrateの挙動も整理必要
