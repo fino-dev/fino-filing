0c2e9b276f78bbcb8993be58e9419c4ee785224c
Akiya-Kudo <k.akiya07@gmail.com>
2026-03-22 12:49:07 +0900

    feat: implement new HttpClient with error handling and rate limiting, and remove deprecated _api_client

--- src/fino_filing/collector/error.py
+++ src/fino_filing/collector/error.py
@@ -12,3 +12,38 @@ class CollectorDateRangeValidationError(FinoFilingException, ValueError):
         )
         self.date_from = date_from
         self.date_to = date_to
+
+
+class HttpRequestError(FinoFilingException):
+    """Raised when HTTP request fails."""
+
+    def __init__(self, url: str, error: Exception):
+        super().__init__(f"HTTP request failed: {url}, error: {error}")
+        self.url = url
+        self.error = error
+
+
+class HttpRateLimitError(FinoFilingException):
+    """Raised when API rate limit is exceeded."""
+
+    def __init__(self):
+        super().__init__("API rate limit exceeded.")
+
+
+class HttpAuthenticationError(FinoFilingException):
+    """Raised when API authentication fails (missing User-Agent)."""
+
+    def __init__(self):
+        super().__init__(
+            "API authentication failed. Please check your credentials is correctly set."
+        )
+
+
+class HttpNotFoundError(FinoFilingException):
+    """Raised when requested resource is not found."""
+
+    def __init__(self, url: str):
+        super().__init__(
+            f"Requested resource not found. Please check your request parameters and try again: {url}"
+        )
+        self.url = url
