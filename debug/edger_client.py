1218be5ad40fa4fc34fa414ba72533101027b768
Akiya-Kudo <k.akiya07@gmail.com>
2026-03-29 18:51:38 +0900

    fixes

--- debug/edger_client.py
+++ debug/edger_client.py
@@ -2,6 +2,10 @@ import json
 
 from fino_filing import EdgerClient, EdgerConfig
 
-# submissions
 client = EdgerClient(EdgerConfig(user_agent_email="odukaki@gmail.com"))
-print(json.dumps(client.get_submissions("0000320193"), indent=4))
+
+# submissions
+# print(json.dumps(client.get_submissions("0000320193"), indent=2))
+
+# facts
+print(json.dumps(client.get_company_facts("0000320193"), indent=2))
