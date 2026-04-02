136891976e1af0e78f1f8781df9f52137c958e90
Akiya-Kudo <k.akiya07@gmail.com>
2026-04-02 23:29:02 +0900

    feat: edgerのhelperのテストを実装

---
+++ test/module/collector/edger/test_edger_helpers.py
@@ -0,0 +1,41 @@
+import pytest
+
+from fino_filing.collector.edger._helpers import (
+    _build_company_facts_json_file_name,
+    _build_recent_submissions_json_file_name,
+)
+
+
+@pytest.mark.module
+@pytest.mark.collector
+@pytest.mark.edger
+class TestEdgerHelpers:
+    class TestBuildRecentSubmissionsJsonFileName:
+        def test_10_cik(self) -> None:
+            """ """
+            assert (
+                _build_recent_submissions_json_file_name("1234567890")
+                == "CIK1234567890-submissions.json"
+            )
+
+        def test_empty_cik(self) -> None:
+            """ """
+            assert (
+                _build_recent_submissions_json_file_name("")
+                == "CIK0000000000-submissions.json"
+            )
+
+    class TestBuildCompanyFactsJsonFileName:
+        def test_10_cik(self) -> None:
+            """ """
+            assert (
+                _build_company_facts_json_file_name("1234567890")
+                == "CIK1234567890-companyfacts.json"
+            )
+
+        def test_empty_cik(self) -> None:
+            """ """
+            assert (
+                _build_company_facts_json_file_name("")
+                == "CIK0000000000-companyfacts.json"
+            )
